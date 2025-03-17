e0128ec37c5d38e2ceadebc1bf946b09137a83a5
RozangelaPeixoto <roxpmelo@gmail.com>
2025-03-17 14:13:45 -0300

    ajustes de css do menu, organização das pastas

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,27 @@
+
+submenu = document.getElementById("submenu");
+list_submenu = document.querySelector(".nav-link--submenu");
+size_menu = 195;
+open_menu = document.getElementById("open-menu");
+close_menu = document.getElementById("close-menu");
+
 document.getElementById("menu").addEventListener("click", function () {
     document.querySelector(".header__nav-list").classList.toggle("open");
+    open_menu.style.display = (elemento.style.display === "none") ? "block" : "none";
+    close_menu.style.display = (elemento.style.display === "none") ? "block" : "none";
 });
+
+if(window.innerWidth-size_menu <= 960){
+    submenu.addEventListener("click",  function () {
+        list_submenu.classList.toggle("open");
+    });
+}else{
+    console.log("entrou no else")
+    submenu.addEventListener("mouseover", function () {
+        list_submenu.classList.add("open");
+    });
+    
+    list_submenu.addEventListener("mouseleave", function () {
+        list_submenu.classList.remove("open");
+    });
+}
